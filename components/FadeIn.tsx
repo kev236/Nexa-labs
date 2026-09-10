7952f1871b192f8686ed279a06ff21eb878e1c5d
kev236 <158450372+kev236@users.noreply.github.com>
2026-09-10 14:17:36 +0200

    commit

--- components/FadeIn.tsx
+++ components/FadeIn.tsx
@@ -1,14 +1,22 @@
 "use client";
+
 import { motion } from "framer-motion";
 import { ReactNode } from "react";
 
-export default function FadeIn({ children, delay = 0 }: { children: ReactNode, delay?: number }) {
+interface FadeInProps {
+  children: ReactNode;
+  delay?: number;
+  className?: string;
+}
+
+export default function FadeIn({ children, delay = 0, className }: FadeInProps) {
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true, margin: "-10%" }}
       transition={{ duration: 0.6, delay, ease: [0.21, 0.47, 0.32, 0.98] }}
+      className={className}
     >
       {children}
     </motion.div>
